646164cf44afd0dbece13ac0f2123e4e42c7805f
busra <b.busraayilmaz@gmail.com>
2023-10-22 17:44:50 +0300

    context-created

---
+++ client/src/contextts/AuthContext.js
@@ -0,0 +1,24 @@
+import { useState, createContext, useEffect, useContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  // ilk giris islemi gerceklestiren fonksiyon
+  const login = (data) => {
+    setLoggedIn(true);
+    setUser(data);
+  };
+  // context içerisinden componentlere gönderilen veriler
+  const values = {
+    loggedIn,
+    user,
+    login,
+  };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = () => useContext(AuthContext);
+export { AuthProvider, useAuth };
