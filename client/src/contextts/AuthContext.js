b1d6825dd2b32f3c2789f4e846fc8f5055c196d4
busra <b.busraayilmaz@gmail.com>
2023-10-24 19:21:32 +0300

    logout-process-added-to-page

--- client/src/contextts/AuthContext.js
+++ client/src/contextts/AuthContext.js
@@ -1,5 +1,5 @@
 import { useState, createContext, useEffect, useContext } from "react";
-import { fetchMe } from "../api";
+import { fetchLogout, fetchMe } from "../api";
 import { Flex, Spinner } from "@chakra-ui/react";
 
 const AuthContext = createContext();
@@ -30,11 +30,24 @@ const AuthProvider = ({ children }) => {
     localStorage.setItem("access-token", data.accessToken);
     localStorage.setItem("refresh-token", data.refreshToken);
   };
+
+  const logout = async (callback) => {
+    setLoggedIn(false);
+    setUser(null);
+
+    await fetchLogout();
+    localStorage.removeItem("access-token");
+    localStorage.removeItem("refresh-token");
+
+    callback();
+  };
+
   // context içerisinden componentlere gönderilen veriler
   const values = {
     loggedIn,
     user,
     login,
+    logout,
   };
   if (loading) {
     return (
