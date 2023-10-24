820f102b80a8a7205879edd7c67f200c0176d5d7
busra <b.busraayilmaz@gmail.com>
2023-10-24 19:19:48 +0300

    whenprofile was-closed-redirected-to-the-home-page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { useAuth } from "../../contextts/AuthContext";
-import { Text } from "@chakra-ui/react";
+import { Text, Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-function Profile() {
-  const { user } = useAuth();
+function Profile({ history }) {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    logout(() => {
+      navigate("/");
+    });
+  };
   return (
     <div>
       <Text fontSize="22">Profile</Text>
       <code>{JSON.stringify(user)}</code>
+      <br />
+      <br />
+
+      <Button colorScheme="pink" variant="solid" onClick={handleLogout}>
+        Logout
+      </Button>
     </div>
   );
 }
