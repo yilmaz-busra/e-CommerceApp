f7b3ca46bd977dda4bf451bda843ba6bd1b6deb2
busra <b.busraayilmaz@gmail.com>
2023-10-28 23:24:36 +0300

    added-whether the product-in-the-cart-has-been-in-the-cart-before

--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -5,9 +5,13 @@ import { fetchProductList, fetchProduct } from "../../api";
 import { Box, Text, Button } from "@chakra-ui/react";
 import moment from "moment";
 import ImageGallery from "react-image-gallery";
+import { useBasket } from "../../contextts/BasketContext";
+
 function ProductDetail() {
   // tıklanan ürüne ait id değerini tutmak için
   const { product_id } = useParams();
+
+  const { addToBasket, items } = useBasket();
 
   const { isLoading, isError, data } = useQuery(["product", product_id], () =>
     fetchProduct(product_id)
@@ -26,9 +30,17 @@ function ProductDetail() {
   console.log(images);
 
   console.log(process.env.REACT_APP_BASE_ENDPOINT);
+
+  // sepete eklenmek istenen ürünün hali hazırda spette olup olmadığını kontrol ediyoruz.
+  const findBasketItem = items.find((item) => item._id === product_id);
   return (
     <div>
-      <Button colorScheme="pink">Add to Basket</Button>
+      <Button
+        colorScheme={findBasketItem ? "pink" : "green"}
+        onClick={() => addToBasket(data, findBasketItem)}
+      >
+        {findBasketItem ? "Remove From Basket" : "Add to Basket"}
+      </Button>
       <Text as="h2" fontSize="2xl">
         {data.title}
       </Text>
